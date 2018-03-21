02597bb0f27a38e6d25b0c238352d25f63542179
Jesus Jimenez <jedar93@gmail.com>
2018-03-20 22:13:40 -0400

    mlab

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     db: {
-        uri: 'mongodb://localhost/delivery'
+        uri: 'mongodb://jedar93:jesusdari0@ds121089.mlab.com:21089/delifood'
     },
     jwt: {
         secret: '360efb01996ab509c3003ab8d5fb4f5ca70e8ba8214a0510c5b8fcd687e1e514'
